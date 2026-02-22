d81b8135a7c5e65a558523d1a718b7003e280e7b
Saksham Luthra <sakshamluthra2016@gmail.com>
2026-02-22 23:57:40 +0530

    video player updated

--- src/Pages/VideoPlayer.jsx
+++ src/Pages/VideoPlayer.jsx
@@ -32,10 +32,16 @@ const VideoPlayer = () => {
           src={`https://vidfast.pro/movie/${id}?autoPlay=false&server=Bollywood`}
           allowFullscreen="true"
         ></iframe> */}
-        <iframe
+        {/* <iframe
           width="100%"
           height="100%"
           src={`https://vidcore.net/movie/${id}`}
+          allowFullscreen="true"
+        ></iframe> */}
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://www.vidking.net/embed/movie/${id}?color=ff0000&autoPlay=true`}
           allowFullscreen="true"
         ></iframe>
       </div>
